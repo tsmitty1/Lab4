487de3d9ab7ffff65f1c171329e9538888279bdb
tylersmith <tylersmith@wifi-roaming-128-4-107-166.host.udel.edu>
2019-04-22 18:49:56 -0400

    Commit 2

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -1,6 +1,11 @@
 package pkgGame;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Random;
 
 import pkgHelper.LatinSquare;
@@ -409,5 +414,165 @@ public class Sudoku extends LatinSquare {
 			ar[index] = ar[i];
 			ar[i] = a;
 		}
+	}
+	private class Cell{
+		
+		private int iRow;
+		private int iCol;
+		private ArrayList<Integer> lstValidValues = new ArrayList<Integer>();
+		
+		public Cell(int iRow, int iCol) {
+			
+			super();
+			this.iRow = iRow;
+			this.iCol = iCol;
+		}
+		
+		private void setCells() {
+			for(int iRow = 0; iRow < iSize; iRow++) {
+				for(int iCol = 0; iCol < iSize; iCol++) {
+					Cell c = new Cell (iRow, iCol);
+					c.setlstValidCalues(getAllValidCellValues (iCol, iRow));
+					c.shuffleValidValues();
+					cells.put(c.hashCode(), c);
+				}
+			}
+		}
+		
+		public int getiRow() {
+			
+			return iRow;
+		}
+		
+		public int getiCol() {
+			
+			return iCol;
+		}
+		
+		private void ShowAvaliableValues() {
+			for(int iRow  = 0; iRow < iSize; iRow++) {
+				for(int iCol = 0; iCol < iSize; iCol++) {
+					
+					Cell c = cells.get(Objects.hash(iRow, iCol));
+					for(Integer i : c.getLstValidValues()) {
+						System.out.print(i+" ");
+					}
+					System.out.println("");
+				}
+			}
+		}
+		
+		private HashSet<Integer> getAllValidValues(int iCol, int iRow){
+			if(this.getPuzzle()[iRow][iCol] > 0) {
+				HashSet<Integer> hs = new HashSet<Integer>();
+				hs.add(this.getPuzzle() [iRow][iCol]);
+				return hs;
+			}
+			
+			HashSet<Integer> hsCellRange = new HashSet<Integer>();
+			for(int i = 0; i < iSize; i++) {
+				hsCellRange.add(i+1);
+			}
+			
+			HashSet<Integer> hsUsedValues = new HashSet<Integer>();
+			Collections.addAll(hsUsedValues, Arrays.stream(super.getRow(iRow)).boxed().toArray(Integer[]::new));
+			Collections.addAll(hsUsedValues, Arrays.stream(super.getColumn(iCol)).boxed().toArray(Integer[]::new));
+			Collections.addAll(hsUsedValues, Arrays.stream(this.getRegion(iCol, iRow)).boxed().toArray(Integer[]::new));
+			
+			hsCellRange.removeAll(hsUsedValues);
+			return hsCellRange;
+		}
+		
+		@Override
+		public int hashCode() {
+			
+			return Objects.hash(iRow, iCol);
+		}
+		
+		@Override
+		public boolean equals (Object o) {
+			
+			if(o == this) {
+				
+				return true;
+			}
+			
+			if(!(o instanceof Cell)) {
+				
+				return false;
+			}
+			Cell c = (Cell) o;
+			return iCol = c.iCol && iRow == c.iRow;
+		}
+		
+		public ArrayList<Integer> getLstValidValues(){
+			
+			return lstValidValues;
+		}
+		
+		public void setlstValidCalues(HashSet<Integer> hsValidValues) {
+			
+			lstValidValues = new ArrayList<Integer> (hsValidValues);
+		}
+		
+		public void shuffleValidValues() {
+			
+			Collections.shuffle(lstValidValues);
+		}
+		
+		public Cell getNextCell(Cell c) {
+			
+			int iCol = c.getiCol() + 1;
+			int iRow = c.getiRow() + 1;
+			int iSqrtSize = (int) Math.sqrt(iSize);
+			
+			if(iCol >= iSize && iRow < iSize - 1) {
+				iRow++;
+				iCol = 0;
+			}
+			
+			if(iRow >= iSize && iCol >= iSize) {
+				return null;
+			}
+			
+			if(iRow < iSqrtSize) {
+				if(iCol < iSqrtSize) {
+					iCol = iSqrtSize;
+				}
+			}
+			else if(iRow < iSize - iSqrtSize) {
+				if(iCol == (int) (iRow / iSqrtSize) * iSqrtSize) {
+					iCol = iCol + iSqrtSize;
+				}
+			}
+			else {
+				if(iCol == iSize - iSqrtSize) {
+					iRow = iRow + 1;
+					iCol = 0;
+					if(iRow >= iSize) {
+						return null;
+					}
+				}
+			}
+			return (Cell) cells.get(Objects.hash(iRow, iCol));
+		}
+		
+		private boolean fillRemaining(Cell c) {
+			if(c == null) {
+				return true;
+			}
+			
+			for(int num: c.getLstValidValues()) {
+				if(isValidValue(c, num)) {
+					this.getPuzzle()[c.getiRow()][c.getiCol()] = 0;
+					
+					if(fillRemaining(c.getNextCell(c))) {
+						this.getPuzzle()[c.getiRow()][c.getiCol()] = 0;
+					}
+				}
+			}
+			return false;
+		}
+		
 	}
 }
